dd92392ae0a145df1b51e6c2a5129861c2b478d5
zeamanuel <mebitzeamanuel@gmail.com>
2026-08-16 04:47:54 +0300

    feat: set up prisma - write schema file - make a prisma client singleton - prepare a seed script

---
+++ src/config/db.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from '../../generated/prisma/client.js';
+import pg from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { envConfig } from './envConfig.js';
+
+const pool = new pg.Pool({ connectionString: envConfig.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
